c38227cb334f20fe8881497c9ef28372a41b5a78
wandersonSantiago <wandersonsantiago86@gmail.com>
2020-07-28 00:57:29 -0300

    commit aula 01

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './App.css';
+import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carrocel from './components/Carousel';
+import Footer from './components/Footer';
+
+
+
+function App() {
+  return (
+    <div style={{ background : "#141414"}}>
+     <Menu />
+
+    <BannerMain 
+      videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+      url={dadosIniciais.categorias[0].videos[0].url}
+      videoDescription={"O que é Front-end? Trabalhando na ares"}
+      />
+
+      <Carrocel 
+       ignoreFirstVideo
+       category={dadosIniciais.categorias[0]} />
+
+      <Carrocel 
+       ignoreFirstVideo
+       category={dadosIniciais.categorias[1]} />
+
+      <Carrocel 
+       ignoreFirstVideo
+       category={dadosIniciais.categorias[2]} />
+
+      <Carrocel 
+       ignoreFirstVideo
+       category={dadosIniciais.categorias[3]} />
+
+
+      <Carrocel 
+       ignoreFirstVideo
+       category={dadosIniciais.categorias[4]} />
+
+
+      <Carrocel 
+       ignoreFirstVideo
+       category={dadosIniciais.categorias[5]} />
+
+      <Footer />
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png'
+import './Menu.css'
+import Button from '../Button';
+
+
+function Menu(){
+
+        return ( 
+           <nav className="Menu">
+               <a href="/">
+                <img className="Logo" src={Logo} alt="wanflix logo" />
+               </a>
+
+               <Button  as="a" className="ButtonLink" href="/" >
+                    Novo video
+               </Button>
+           </nav>
+         );
+
+}
+ 
+export default Menu;
